2b2a22c066adf747bf08005b4d01ebb423e40743
Bautista1999 <juan@anysphere.co>
2025-04-23 19:11:19 -0300

    latest changes!

--- lib/domain/entities/feature_entity.dart
+++ lib/domain/entities/feature_entity.dart
@@ -4,13 +4,15 @@ class FeatureEntity extends Equatable {
   final String id;
   final String name;
   final String description;
+  final List<String> labels;
 
   const FeatureEntity({
     required this.id,
     required this.name,
     required this.description,
+    this.labels = const [],
   });
 
   @override
-  List<Object?> get props => [id, name, description];
+  List<Object?> get props => [id, name, description, labels];
 }
